aac08536431663eb881aeb5a38a65ec7fbb4d790
Maxim Chistov <m.chistov@fundcount.com>
2015-07-11 18:16:22 +0300

    enabling security

--- src/main/java/habraspring/config/MvcConfig.java
+++ src/main/java/habraspring/config/MvcConfig.java
@@ -13,7 +13,5 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-     //   registry.addViewController("/hello").setViewName("hello");
-      //  registry.addViewController("/login").setViewName("login");
     }
 }
---
+++ src/main/java/habraspring/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package habraspring.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll();
+    }
+}
