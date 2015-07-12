1c5894f87b4f7d3027c6cd8cd428a498ebd8b63b
Maxim Chistov <m.chistov@fundcount.com>
2015-07-12 16:34:27 +0300

    show users to admins(with mistake)

--- src/main/java/habraspring/controllers/UsersController.java
+++ src/main/java/habraspring/controllers/UsersController.java
@@ -1,8 +1,15 @@
 package habraspring.controllers;
 
+import habraspring.entities.User;
+import habraspring.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Maxim Chistov on 12.07.15.
@@ -11,5 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 @PreAuthorize("hasRole('ADMIN')")
 public class UsersController {
+    @Autowired
+    UsersRepository users;
 
+    @RequestMapping(method = RequestMethod.GET)
+    public List<User> getUsers()
+    {
+        List<User> result = new ArrayList<>();
+        users.findAll().forEach(result::add);
+        return result;
+    }
 }
