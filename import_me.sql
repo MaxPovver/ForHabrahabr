4142b872127885af3c31056303a7502f023aa417
Maxim Chistov <m.chistov@fundcount.com>
2015-07-12 17:39:30 +0300

    db structure for this exampe

--- import_me.sql
+++ import_me.sql
@@ -7,7 +7,7 @@
 #
 # Адрес: 127.0.0.1 (MySQL 5.6.23)
 # Схема: forhabrahabr
-# Время создания: 2015-07-11 16:22:22 +0000
+# Время создания: 2015-07-12 14:38:48 +0000
 # ************************************************************
 
 
@@ -20,6 +20,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
+# Дамп таблицы roles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `role` (`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+
+INSERT INTO `roles` (`id`, `role`)
+VALUES
+	(1,'ADMIN'),
+	(3,'GUEST'),
+	(2,'USER');
+
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
 # Дамп таблицы users
 # ------------------------------------------------------------
 
@@ -30,7 +56,8 @@ CREATE TABLE `users` (
   `username` varchar(250) DEFAULT NULL,
   `password` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `users` WRITE;
@@ -38,9 +65,39 @@ LOCK TABLES `users` WRITE;
 
 INSERT INTO `users` (`id`, `username`, `password`)
 VALUES
-	(1,'user','user');
+	(1,'user','user'),
+	(2,'another','lol'),
+	(4,'userandadmin','123');
 
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Дамп таблицы users_roles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users_roles`;
+
+CREATE TABLE `users_roles` (
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `role_id` bigint(20) unsigned DEFAULT NULL,
+  KEY `hasuser` (`user_id`),
+  KEY `hasrole` (`role_id`),
+  CONSTRAINT `hasrole` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `hasuser` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `users_roles` WRITE;
+/*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
+
+INSERT INTO `users_roles` (`user_id`, `role_id`)
+VALUES
+	(1,1),
+	(2,2),
+	(4,1),
+	(4,2);
+
+/*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
