7daeee422937da0b6a210d16715bc6a0c59c5057
Maxim Chistov <m.chistov@fundcount.com>
2015-07-12 17:27:11 +0300

    user specific commands

--- src/main/java/habraspring/controllers/UsersController.java
+++ src/main/java/habraspring/controllers/UsersController.java
@@ -4,6 +4,7 @@ import habraspring.entities.User;
 import habraspring.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,8 +36,10 @@ public class UsersController {
     @RequestMapping(method = RequestMethod.POST)
     public User addUser(String username, String password, String password_confirm)
     {
+        //no empty fields allowed
         if (username.isEmpty() || password.isEmpty() || password_confirm.isEmpty())
             return null;
+        //passwords should match
         if (!password.equals(password_confirm))
             return null;
         return users.save(new User(username, password));
@@ -46,5 +49,17 @@ public class UsersController {
     public ModelAndView getUserForm()
     {
         return new ModelAndView("add");
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Long id)
+    {
+        users.delete(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable("id") Long id)
+    {
+        return users.findOne(id);
     }
 }
