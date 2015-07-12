116b484c98265619c2d67e6dd98e8faad070f0c3
Maxim Chistov <m.chistov@fundcount.com>
2015-07-12 16:16:06 +0300

    adding new controller

---
+++ src/main/java/habraspring/controllers/UsersController.java
@@ -0,0 +1,12 @@
+package habraspring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Maxim Chistov on 12.07.15.
+ */
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+}
